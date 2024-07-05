8864122ae727feb84de4a43e1942b1726b7e7725
LJHJH <160108023+JHL222@users.noreply.github.com>
2024-07-05 14:49:07 +0900

    메인페이지 수정

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import { BrowserView, MobileView } from 'react-device-detect'
 import DesktopMain from './browser/DesktopMain';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import MobileMain from './mobile/MobileMain';
-import Header from './browser/BrowserHeader';
-=======
-import MobileApp from './mobile/MobileApp';
->>>>>>> e8c3b60 (고병현)
-=======
-=======
->>>>>>> 8874dd3 (Header 추가)
-import MobileApp from './mobile/MobileApp';
-=======
-=======
-import Header from './browser/BrowserHeader';
->>>>>>> 09b5dc5 (Header 추가)
 import MobileMain from './mobile/MobileMain';
 import Header from './browser/BrowserHeader';
 
->>>>>>> e99a4de (.)
->>>>>>> cbf3914 (.)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8c1841e (고병현 app.js  moblieMain 2개인거 수정#2)
 function App() {
   return (
     <>
       <BrowserView>
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -74,10 +53,13 @@ function App() {
 >>>>>>> c20677a (feat(main))
 >>>>>>> 33ac7f4 (feat(main))
 >>>>>>> acdd4b8 (feat(main))
+=======
+        <Header/>
+>>>>>>> 9041ac6 (메인페이지 수정)
         <DesktopMain />
       </BrowserView>
       <MobileView>
-        <MobileApp />
+        <MobileMain />
       </MobileView>
     </>
   );
