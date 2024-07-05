2763bb7a77cf987196ffedc4c7209e69bb6abdac
BH0120 <kbh9757@naver.com>
2024-07-05 10:34:09 +0900

    고병현 app.js  moblieMain 2개인거 수정#2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import DesktopMain from './browser/DesktopMain';
 import MobileMain from './mobile/MobileMain';
 import Header from './browser/BrowserHeader';
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8c1841e (고병현 app.js  moblieMain 2개인거 수정#2)
 function App() {
   return (
     <>
