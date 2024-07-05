78896dc8659ac88ee03c396323dba406ef4e65cc
JangSE123 <jse010206@gmail.com>
2024-07-05 13:57:24 +0900

    feat(main)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { BrowserView, MobileView } from 'react-device-detect'
 import DesktopMain from './browser/DesktopMain';
 import Header from './browser/BrowserHeader';
 import MobileMain from './mobile/MobileMain';
+import Header from './browser/BrowserHeader';
 
 
 function App() {
   return (
     <>
       <BrowserView>
+<<<<<<< HEAD
         <Header />
+=======
+        <Header/>
+>>>>>>> c20677a (feat(main))
         <DesktopMain />
       </BrowserView>
       <MobileView>
--- src/browser/DesktopMain.js
+++ src/browser/DesktopMain.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import styles from './DesktopMain.module.css';
 
@@ -7,8 +8,39 @@ function DesktopMain(props) {
            <div className={styles.main}>
             
            </div>
+=======
+import React, { useState } from 'react'
+import styles from './DesktopMain.module.css'
+// import image1 from './a.jpg'
+// import image2 from './b.jpg'
+
+function DesktopMain() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const [imageHeight, setImageHeight] = useState('100vh');
+
+    const handleScroll = () => {
+        const position = window.scrollY;
+
+        if(position > 0) {
+            setImageHeight('0');
+        } else {
+            setImageHeight('100vh');
+        }
+        setScrollPosition(position);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+  return (
+    <div className={styles.main}>
+        <div className={styles.image} style={{ height: imageHeight }}/>
+        <div className={`${styles.content} ${scrollPosition > 0 ? styles.show : ''}}`}>
+            {}
+            <h1>메인 내용</h1>
+>>>>>>> c20677a (feat(main))
         </div>
-    );
+    </div>
+  )
 }
 
 export default DesktopMain;
