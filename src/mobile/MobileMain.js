719bae0fbd2b559fa8a2ea6d9862f102e90bbfee
hanjunnn <qwedfr79@naver.com>
2024-07-05 12:06:01 +0900

    Feat(GitHubCalendar)

--- src/mobile/MobileMain.js
+++ src/mobile/MobileMain.js
@@ -1,10 +1,37 @@
 import React from 'react';
-
+import GitHubCalendar from 'react-github-calendar';
+const selectMonth = contributions => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 4;
+  
+    return contributions.filter(activity => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+  
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
 function MobileMain(props) {
     return (
         <div>
-            MobileMain.js
-        </div>
+<GitHubCalendar 
+colorScheme	="light"
+  username="hanjunnn" 
+  blockSize	= {18}
+  transformData={selectMonth} 
+  hideColorLegend
+  hideTotalCount
+  
+//   labels={{
+//     totalCount: '{{count}} contributions in the last half year',
+//   }}
+/>
+</div>
     );
 }
 
