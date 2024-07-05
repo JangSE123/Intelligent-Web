4d08d6fe460ce0dc837b430e1c1448955c0eedaa
BH0120 <kbh9757@naver.com>
2024-07-05 17:06:31 +0900

    스크롤바 수정완료 padding 값 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserView, MobileView } from 'react-device-detect'
 import MobileApp from './mobile/MobileApp';
 import DesktopApp from './browser/DesktopApp';
-import './App.css';
+
 
 function App() {
   return (
--- src/browser/DesktopApp.js
+++ src/browser/DesktopApp.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import DesktopMain from './DesktopMain'
 import Header from './BrowserHeader'
-import styles from './DesktopApp.module.css'
 
 export default function DesktopApp() {
   return (
