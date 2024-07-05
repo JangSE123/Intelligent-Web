5604703c528d03130e455ab454259b92931a2588
BH0120 <kbh9757@naver.com>
2024-07-05 16:03:46 +0900

    고병현 css 이정현ㅇ ㅣㅁ똥싼거 치움

--- src/browser/DesktopApp.js
+++ src/browser/DesktopApp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import DesktopMain from './DesktopMain'
 import Header from './BrowserHeader'
+import styles from './DesktopApp.module.css'
 
 export default function DesktopApp() {
   return (
