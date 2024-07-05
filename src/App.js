8c1841e7e9bdb0171c770499d921e6cc32d55f35
BH0120 <kbh9757@naver.com>
2024-07-05 10:34:09 +0900

    고병현 app.js  moblieMain 2개인거 수정#2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { BrowserView, MobileView } from 'react-device-detect'
 import DesktopMain from './browser/DesktopMain';
 import MobileMain from './mobile/MobileMain';
 
-import MobileMain from "./component/MobileMain";
-
 function App() {
   return (
     <>
